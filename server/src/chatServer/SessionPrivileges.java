42e2866ef5033d7aa818b759fb7f0d16cb3c6af4
Piotr K <rennoo@gmail.com>
2010-05-30 18:12:19 +0200

    link to docs about bit masks

--- server/src/chatServer/SessionPrivileges.java
+++ server/src/chatServer/SessionPrivileges.java
@@ -5,6 +5,9 @@ package chatServer;
  * Date: 2010-05-30
  * Time: 17:52:34
  */
+
+//  http://www.vipan.com/htdocs/bitwisehelp.html
+    
 public final class SessionPrivileges {
     static final int user = 1;    // 2^^0    000...00000001
     static final int admin = 2;    // 2^^1    000...00000010
