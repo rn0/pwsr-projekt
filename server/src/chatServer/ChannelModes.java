ba9c9151e20a75b6058d423aadd4219709257fd4
Piotr K <rennoo@gmail.com>
2010-06-02 21:01:59 +0200

    channel modes

---
+++ server/src/chatServer/ChannelModes.java
@@ -0,0 +1,12 @@
+package chatServer;
+
+/**
+ * User: Piotr Kapera
+ * Date: 2010-06-02
+ * Time: 20:59:46
+ */
+public enum ChannelModes {
+    s, // Secret channel—not shown in channel list or user whois except to users already on the channel
+    m, // Channel is moderated (only those who hold operator or voice status on the channel can send messages to it)
+    l, // Limit number	Limits number of users able to be on channel (when full, no new users can join)
+}
