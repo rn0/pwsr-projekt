f4f75ca71801132bfe5e1574716a4e5feb347fb4
Piotr K <rennoo@gmail.com>
2010-06-05 22:58:57 +0200

    /names

--- server/src/chatServer/command/handlers/Names.java
+++ server/src/chatServer/command/handlers/Names.java
@@ -1,5 +1,6 @@
 package chatServer.command.handlers;
 
+import chatServer.Channel;
 import chatServer.Server;
 import chatServer.Session;
 import chatServer.command.BaseCommand;
@@ -15,11 +16,19 @@ import java.util.Vector;
 public class Names extends BaseCommand {
     @Override
     public void execute(Server server, Session session, String[] params) {
-        Vector<String> names = new Vector<String>();
-        for (Session s : server.getSessions()) {
-            names.add(s.getNick());
+        if(params.length == 1) {
+            server.send(new Notice(session, server.getSessions().toString()));
         }
-        server.send(new Notice(session, names.toString()));
+        else if(params.length == 2) {
+            Channel chan = server.findChannel(params[1]);
+            if(chan == null) {
+                server.send(new Notice(session, "Unknown channel: " + params[1]));
+            }
+            else {
+                server.send(new Notice(session, server.getSessions(chan).toString()));
+            }
+        }
+
     }
 
     @Override
