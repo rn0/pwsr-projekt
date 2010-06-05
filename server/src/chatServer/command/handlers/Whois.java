ab3fe741605780547402b5da25059f568f41e0b7
Piotr K <rennoo@gmail.com>
2010-06-06 00:54:28 +0200

    /whois command

---
+++ server/src/chatServer/command/handlers/Whois.java
@@ -0,0 +1,31 @@
+package chatServer.command.handlers;
+
+import chatServer.Server;
+import chatServer.Session;
+import chatServer.command.BaseCommand;
+import chatServer.message.Notice;
+
+/**
+ * User: Piotr Kapera
+ * Date: 2010-06-06
+ * Time: 00:42:17
+ */
+public class Whois extends BaseCommand {
+    @Override
+    public void execute(Server server, Session session, String[] params) {
+        if(params.length == 2) {
+            Session about = server.findSession(params[1]);
+            if(about == null) {
+                server.send(new Notice(session, "Unknown user: " + params[1]));
+            }
+            else {
+                server.send(new Notice(session, "Info about: " + about.getNick() + "; Channels: " + about.getChannels().toString()));
+            }
+        }
+    }
+
+    @Override
+    public String getUsage() {
+        return "/names";
+    }
+}
